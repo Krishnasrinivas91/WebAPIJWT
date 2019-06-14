df6288f007897110fcd4c2ba245567edc3204d3e
Krishna Srinivas Arveti <krishnasrinivas91@gmail.com>
2019-06-14 18:23:20 +0530

    Web API with Json web tokens
    .Net Core Web API with Json Web tokens

---
+++ WebApiJWTs_GitHub/Controllers/LoginController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using WebApiJWTs_GitHub.Models;
+
+namespace WebApiJWTs_GitHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config { get; }
+        public LoginController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody]UserModel userModel)
+        {
+            IActionResult response = Unauthorized();
+            var user = AuthenticateUser(userModel);
+
+            if (user != null)
+            {
+                var tokenString = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenString });
+            }
+            return response;
+        }
+
+        private string GenerateJSONWebToken(object user)
+        {
+            var securityKey = JwtSecurity.JwtSecurityKey.Create(_config["Jwt:Key"]);
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"], _config["Jwt:Issuer"], null, expires: DateTime.Now.AddMinutes(2),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private UserModel AuthenticateUser(UserModel userModel)
+        {
+            if (userModel != null && userModel.UserName == "TestUser")
+            {
+                return new UserModel { UserName = "TestUser", UserEmail = "Test@Test.com" };
+            }
+            return null;
+        }
+    }
+}
---
+++ WebApiJWTs_GitHub/Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiJWTs_GitHub.Models
+{
+    public class UserModel
+    {
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+    }
+}
